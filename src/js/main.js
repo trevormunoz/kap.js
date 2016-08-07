51e6241d1b7ded1611270f348f21e2c29fb7d473
Trevor Muñoz <trevor@trevormunoz.com>
2016-08-06 20:45:01 -0400

    FIXME: Temporary workaround for broken webpack string parsing
    This problem should go away after a new release of css-loader gets cut
    that includes css-selector-tokenizer >= 0.6.0
    
    See https://github.com/webpack/css-loader/pull/307

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Widget from './Widget';
 
+import "../scss/main.scss";
 window.console.log("Webpack works!");
 
 ReactDOM.render(
